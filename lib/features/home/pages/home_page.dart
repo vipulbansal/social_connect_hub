c1a4d08a2101e2e1ba7ade461063ec1e95fe9054
vipul bansal <vipulbansalkkp@gmail.com>
2025-04-24 11:11:28 +0530

    home screen

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../auth/services/auth_service.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -13,10 +12,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: SafeArea(child: Container(
-        child: Center(child: Text('Home ${context.read<AuthService>().currentUser?.name}'),),
-      )),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Consumer<AuthService>(
+            builder: (context, provider, child) {
+              if (provider.isLoading) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                return Center(
+                  child: Text('Home ${provider.currentUser?.name}'),
+                );
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
